09317bc1ea287c819c4c55d455b9bd4a9c5e8354
romeyiow <jimperialyosh@gmail.com>
2024-12-11 20:23:25 +0800

    Done with UI ONLY sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import HeaderBar from "./components/reusableComponents/HeaderBar"
+import HeaderBar from "./components/reusableComponents/HeaderBar";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { AppSidebar } from "./components/reusableComponents/AppSidebar";
 
 
-function App() {
+// function Layout() {
+//   return (
+//     <div className="flex h-screen">
+//       <HeaderBar />
+//       <SidebarProvider>
+//         <AppSidebar />
+//       </SidebarProvider>
+//       <main className="flex-1 p-4">
 
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App({ children }) {
   return (
-    <>
-      <HeaderBar />
-    </>
-  )
+    
+    <SidebarProvider>
+      <div style={{ display: "flex" }}>
+        <AppSidebar />
+        <main style={{ flex: 1 }}>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </div>
+    </SidebarProvider>
+  );
 }
-
-export default App
