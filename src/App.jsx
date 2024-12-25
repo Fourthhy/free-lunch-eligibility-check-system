cd5c6e08118cc82082d40584f4258cda7b381030
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-25 02:45:09 +0000

    Staged Changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>+</button>
-        <button onClick={() => setCount((count) => count - 1)}>-</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
-          count is { count }
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
