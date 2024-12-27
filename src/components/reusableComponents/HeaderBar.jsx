274180dac1a26f7badabcfd3105bc61b2bc73b90
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-27 05:40:36 +0000

    Fixed responsiveness for phase 1

--- src/components/reusableComponents/HeaderBar.jsx
+++ src/components/reusableComponents/HeaderBar.jsx
@@ -1,24 +1,28 @@
 function HeaderBar() {
     return (
         <>
-            <div className="h-[145px] w-screen bg-[#1F3463] flex text-white">
+            <div className="h-[110px] w-screen bg-[#1F3463] flex text-white">
                 <div className=" h-[100%] w-[50%] flex">
+
                     <div className="h-[100%] flex flex-col items-center justify-center">
-                        <img src="/logo.svg" alt="" className="h-[116px] w-[121px] " />
+                        <img src="/logo.svg" alt="" className="h-[100px] w-[100px] " />
                     </div>
 
                     <div className="h-[100%] w-[100%] flex flex-col justify-center content-center">
+
                         <div className="w-[100%]">
-                            <img src="/header-text.svg" alt="" className="h-[42px] w-[468px]" />
+                            <img src="/header-text.svg" alt="" className="h-[30px]" />
                         </div>
+
                         <div className="w-[100%]">
-                            <img src="/sub-header-text.svg" alt="" className="h-[15px] w-[381px]" />
+                            <img src="/sub-header-text.svg" alt="" className="h-[15px]" />
                         </div>
+
                     </div>
                 </div>
 
                 <div className="h-[100%] w-[50%] flex justify-end items-center mr-[30px]">
-                    <img src="/motto-text.svg" alt="" className="h-[34] w-[355]" />
+                    <img src="/motto-text.svg" alt="" className="h-[25] w-[355]" />
                 </div>
             </div>
         </>
