1090b85e25eac392eb93a78932d628a1de48f14c
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-30 05:57:51 +0000

    Checked code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import KStaffPage from "./components/KStaffPage"
+import KStaffPageVersion2 from "./components/KStaffPageVersion2"
 import LoginPage from "./components/LoginPage"
 
 function App() {
