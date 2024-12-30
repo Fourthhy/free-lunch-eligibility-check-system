e019ad6379748172627272d925553dc3f90dfd8e
Fourthhy <miguelmanabo4@gmail.com>
2024-12-30 14:56:35 +0800

    merged with kstaff - fixing issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.webp'],
   plugins: [react()],
   resolve: {
     alias: {
