89a724783b50ff18bb99458a038d2f1ab4051526
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-01-02 14:34:33 +0000

    Separated tempalte

---
+++ src/components/reusableComponents/AdminTemplate.jsx
@@ -0,0 +1,38 @@
+import HeaderBar from "./components/reusableComponents/HeaderBar";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { AppSidebar } from "./components/reusableComponents/AppSidebar";
+
+
+// function Layout() {
+//   return (
+//     <div className="flex h-screen">
+//       <HeaderBar />
+//       <SidebarProvider>
+//         <AppSidebar />
+//       </SidebarProvider>
+//       <main className="flex-1 p-4">
+
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function AdminTemplate({ children }) {
+  return (
+    <>
+      <HeaderBar />
+      <SidebarProvider>
+        <div style={{ display: "flex" }}>
+          <AppSidebar />
+          <main style={{ flex: 1 }}>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </div>
+      </SidebarProvider>
+    </>
+
+  );
+}
