a855b919857b3dd33e620d520cbb8638be5dc2ad
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-01-08 16:45:59 +0000

    Rendered stacked bar chart

--- src/app/app-contents/insights/Daily.jsx
+++ src/app/app-contents/insights/Daily.jsx
@@ -1,7 +1,14 @@
-export default function Insights() {
-    return (
-        <>
-            Insights
-        </>
-    )
+import React from 'react';
+import Example from "./Recharts"
+
+class Daily extends React.Component {
+    render() {
+        return (
+            <>
+                <Example />
+            </>
+        )
+    }
 }
+
+export default Daily
