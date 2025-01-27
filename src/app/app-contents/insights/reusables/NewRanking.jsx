aebc491563d1d97a9c204b57e9a1962b5ba6873d
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-01-27 10:57:14 +0000

    Finalization

--- src/app/app-contents/insights/reusables/NewRanking.jsx
+++ src/app/app-contents/insights/reusables/NewRanking.jsx
@@ -19,9 +19,9 @@ import {
 
 const chartData = [
     { dish_name: "Caldereta", claimed: 150, mobile: 200 },
-    { dish_name: "Abobo", claimed: 105, mobile: 80 },
+    { dish_name: "Adobo", claimed: 105, mobile: 80 },
     { dish_name: "Monggo", claimed: 100, mobile: 120 },
-    { dish_name: "hatdog", claimed: 70, mobile: 130 },
+    { dish_name: "hotdog", claimed: 70, mobile: 130 },
 ]
 
 const chartConfig = {
